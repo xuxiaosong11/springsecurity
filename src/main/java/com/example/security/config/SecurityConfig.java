44df52a405886b5e5271d8ada9707fb568876a0d
qp <2457476273@qq.com>
2020-05-28 14:05:52 +0800

    second commit

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.example.security.config;
 
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity  // 开启
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    /**
+     *
+     * @param http 授权
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -18,5 +25,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/level2/**").hasRole("vip2")
                 .antMatchers("/level3/**").hasRole("vip3")
         ;
+        http.formLogin();
+        http.logout().logoutSuccessUrl("/");
+    }
+
+
+    /**
+     * 认证
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("qp").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1","vip2")
+                .and()
+                .withUser("root").password("root").roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password("123").roles("vip1");
     }
 }
